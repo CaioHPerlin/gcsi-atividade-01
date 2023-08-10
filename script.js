81c20ba16939339f31af25ff758622d81a5bfdb1
Caio H Perlin <caiohperlin@gmail.com>
2023-08-10 03:54:42 -0400

    fix: typewrite function no longer rewrites

--- script.js
+++ script.js
@@ -3,26 +3,32 @@ const jsConfetti = new JSConfetti();
 const onReady = () => {
     let clickable = document.getElementsByClassName("clickable")[0];
     clickable.addEventListener("click", () => {
-        console.log("click");
         jsConfetti.addConfetti();
+        const main = document.getElementsByTagName("main")[0];
+        typewrite(document.getElementsByTagName("main")[0], "VALEU! meu mano tmj agradeço por ter clicado tlgd");
     });
 }
 
 const typewrite = (element, str=element.textContent, spd = 50) => {
-    const backup = element.innerHTML;
-    element.innerHTML = "";
+    if(!element.className.includes("done")){
+        const backup =  element.innerHTML;
+        element.textContent = "";
+        element.innerHTML = "";
 
-    let i = 0; 
-    const interval = setInterval(() => {
-        element.innerHTML += str[i];
-        i++;
+        let i = 0; 
+        const interval = setInterval(() => {
+            element.textContent += str[i];
+            i++;
 
-        if(i == str.length){ 
-            element.innerHTML = backup;
-            clearInterval(interval);
-            onReady();
-        }
-    }, spd);
+            if(i == str.length){ 
+                if(backup != "") { element.innerHTML = backup }
+                clearInterval(interval);
+                element.className += " done";
+                console.log(element.className)
+                onReady();
+            }
+        }, spd);
+    }
 }
 
 const typeQueue = document.getElementsByClassName("typewrite");
