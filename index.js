73f29347d8999ea7ad8f1375cd72103e53af158c
Caio H Perlin <caiohperlin@gmail.com>
2023-08-10 00:13:49 -0400

    refactor: summarize typewrite function

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 let headerText = "se pa... que n sei oq... sei la...";
 typewrite(document.getElementById("foo-text"), headerText);
 
-function typewrite(element, str) {
-    let index = 0;
-    addChar();
-
-    function addChar() {
-        element.innerHTML += str[index];
-        index++;
-        const timeout = setTimeout(addChar, 50);
-
-        if(index >= str.length){
-            clearTimeout(timeout);
-        }
-    }
+function typewrite(element, str, spd = 50) {
+    let i = 0; 
+    const interval = setInterval(() => {
+        element.innerHTML += str[i];
+        i++;
+        if(i == str.length) clearInterval(interval);
+    }, spd);
 }
